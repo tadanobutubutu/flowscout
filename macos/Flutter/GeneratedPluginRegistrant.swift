5a63e8b1947e1c1921065487c5c3babdca9ac59a
tadanobutubutu <58510538+tadanobutubutu@users.noreply.github.com>
2026-06-03 06:47:27 +0900

    fix: update dependencies - flutter_secure_storage 10.x (SPM), drop CocoaPods

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,14 +6,14 @@ import FlutterMacOS
 import Foundation
 
 import app_links
-import flutter_secure_storage_macos
+import flutter_secure_storage_darwin
 import package_info_plus
 import share_plus
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
-  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
